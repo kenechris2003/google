abd53fa37fa728645db8a779c0726c7e519082f9
kenechris2003 <68512941+kenechris2003@users.noreply.github.com>
2025-06-10 11:44:59 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Card from "@/components/card";
+
 import Content from "@/components/content";
 import Header from "@/components/header";
-import Image from "next/image";
 
 export default function Home() {
 
